d0b6200edd79ebc20c29472dc0005813f545ef4c
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-07-12 22:13:08 -0400

    Added comments for layout in js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Take input from text area
+// When search button is pressed grab ^ value
+// use API to get location details as lat and long
+// use api to get 5-day weather info
+// display todays weather in main box
+// display 5 days in smaller boxes
+// save city lat and long in local storage and create button for it
